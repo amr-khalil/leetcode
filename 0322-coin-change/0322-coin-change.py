016aeb0f14e99a9733884059d4194f7f1ddf8f1e
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-11 22:53:19 +0200

    Time: 723 ms (81.28%), Space: 16.9 MB (78.22%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        fewest number of coins 
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+        Base case: dp[0] = 0
+
+        Example:
+        coins = [5]. amount = 10 then:
+        dp[10] = dp[5] + 1
+        dp[5] = dp[0] + 1
+        dp[0] = 0
+        """
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[-1] if dp[-1] != float('inf') else -1
