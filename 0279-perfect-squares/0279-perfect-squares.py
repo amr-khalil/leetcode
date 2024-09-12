5f76413e8ff09575ad6a8d324ec4bb219efab723
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-12 09:03:13 +0200

    Time: 2102 ms (75.56%), Space: 16.6 MB (80.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,18 +1,19 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        # dp[i] stores the minimum number of perfect squares that sum to 'i'
         # dp[i] = min(dp[i], dp[i - j*j] + 1)
-        # dp[4]= min(dp[4],dp[4−1*1]+1) = min(dp[4], dp[3]+1)
-        # dp[4]= min(dp[4],dp[4−2*2]+1) = min(dp[4], dp[0]+1)
-        # Base case: dp[0] = 0
+        # Example: dp[4]= min(dp[4], dp[4−1*1]+1) = min(dp[4], dp[3]+1)
+        #          dp[4]= min(dp[4], dp[4−2*2]+1) = min(dp[4], dp[0]+1)
+        # Base case: dp[0] = 0 (it takes 0 squares to make 0)
 
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
 
-        for i in range(1, n + 1):
-            j = 1
-            while j**2 <= i:
-                dp[i] = min(dp[i], dp[i - j**2] + 1)
-                j += 1
+        squares = [i**2 for i in range(1, int(math.sqrt(n))+ 1)]
+        
+        for square in squares:
+            for i in range(square, n + 1):
+                dp[i] = min(dp[i], dp[i- square] + 1)
 
-        print(dp)
         return dp[-1]
+
