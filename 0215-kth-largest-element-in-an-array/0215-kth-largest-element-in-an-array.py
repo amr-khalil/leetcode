44d234993cd76a9e67d0464c5f971498ecca4ac2
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-23 13:58:26 +0200

    Time: 480 ms (50.98%), Space: 29.6 MB (67.8%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[-k]
-"""
-pq = []
-for n in nums:
-    heapq.heappush(pq, n)
-    if len(pq) > k:
-        heapq.heappop(pq)
-return pq[0]
-"""
+        # Heap solution O(n⋅log k) is generally more efficient than sorting O(n⋅log n),
+        # particularly when k is small compared to n.
+        # If k is large or k is close to n, the efficiency difference is less significant,
+        # but the heap approach will still save memory O(k).
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
