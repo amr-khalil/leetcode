bb4aa5fe12b0977ef23965cdf215c8d892113966
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-24 10:08:18 +0200

    Time: 72 ms (31.9%), Space: 17.8 MB (47.71%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -3,13 +3,14 @@ class Solution:
         # [10,3,8,9,4]
         max_heap = [(-num, idx) for idx, num in enumerate(score)]
         heapify(max_heap)
+
         medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
         rank = 0
         
         while max_heap:
             _, idx = heappop(max_heap)
-            
-            if rank < 3:
+
+            if rank < len(medals):
                 score[idx] = medals[rank]
             else:
                 score[idx] = str(rank + 1)
