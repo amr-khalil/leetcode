6d1e69b447994d859f8c5afd7f68bdc652c87c08
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-24 12:19:50 +0200

    Time: 68 ms (72.22%), Space: 16.9 MB (39.74%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -1,9 +1,14 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        freq = defaultdict(list)
+        ans = []
+        scores = defaultdict(list)
         for ID, score in items:
-            freq[ID].append(score)
-        print(freq)
-        ans =  [[ID, sum(sorted(scores, reverse=True)[:5]) //5] for ID, scores in freq.items()]
-        ans.sort()
+            scores[ID].append(score)
+
+        for ID in scores:
+            top_five = heapq.nlargest(5, scores[ID])
+            top_five_avg = sum(top_five) // 5
+            ans.append([ID, top_five_avg])
+        
+        ans.sort(key=lambda x: x[0])
         return ans
