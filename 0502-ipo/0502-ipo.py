fc3e36e6fd3a908e1c44418a4398092b0b230bab
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-25 14:22:42 +0200

    Time: 1184 ms (28.91%), Space: 43.8 MB (7.47%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,19 +1,20 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
         projects = list(zip(capital, profits))
-        n = len(projects)
-        projects.sort()
-        max_heap = []
+        min_heap = [(capital, profit) for capital, profit in projects] # for capital
+        heapify(min_heap)
+        max_heap = [] # for profit
 
-        ptr = 0
+        # Perform up to k projects
         for i in range(k):
-            while ptr < n and projects[ptr][0] <= w:
-                heappush(max_heap, -projects[ptr][1])
-                ptr += 1
-
+            while min_heap and min_heap[0][0] <= w:
+                # Move all projects that are affordable (capital requirement <= current capital w) to the profit heap
+                capital, profit = heappop(min_heap)
+                # Push the profit into the max-heap (use negative to simulate a max-heap)
+                heappush(max_heap, -profit)
+            # If no project is affordable, we break
             if not max_heap:
                 break
-            
-            w += -heappop(max_heap)
-
+            # Pop the most profitable project and increase the capital
+            w += - heappop(max_heap)
         return w
