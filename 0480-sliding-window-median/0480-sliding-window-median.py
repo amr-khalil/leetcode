77eb2038a9f17697c76cefd8acb8d7d8fa87ba51
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2024-09-25 22:02:22 +0200

    Time: 277 ms (66.8%), Space: 29.7 MB (35.63%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -1,17 +1,54 @@
-import bisect
+import heapq
+
 class Solution:
+    def find_median(self, max_heap, min_heap, heap_size):
+        if heap_size % 2 == 1:
+            return -max_heap[0]
+        else:
+            return (-max_heap[0] + min_heap[0]) / 2
+
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        n = len(nums)
-        ans = []
-        winList = []
-        for i in range(n):
-            if i >= k:
-                winList.remove(nums[i-k])
-            bisect.insort(winList, nums[i])
+        max_heap = []
+        min_heap = []
+        heap_dict = defaultdict(int)
+        result = []
         
-            if i >= k - 1:
-                if k % 2 == 1:
-                    ans.append(winList[k//2])
-                else:
-                    ans.append((winList[k//2 - 1] + winList[k//2]) / 2)
-        return ans
+        for i in range(k):
+            heappush(max_heap, -nums[i])
+            heappush(min_heap, -heappop(max_heap))
+            if len(min_heap) > len(max_heap):
+                heappush(max_heap, -heappop(min_heap))
+        
+        median = self.find_median(max_heap, min_heap, k)
+        result.append(median)
+        
+        for i in range(k, len(nums)):
+            prev_num = nums[i - k]
+            heap_dict[prev_num] += 1
+
+            balance = -1 if prev_num <= median else 1
+            
+            if nums[i] <= median:
+                balance += 1
+                heappush(max_heap, -nums[i])
+            else:
+                balance -= 1
+                heappush(min_heap, nums[i])
+            
+            if balance < 0:
+                heappush(max_heap, -heappop(min_heap))
+            elif balance > 0:
+                heappush(min_heap, -heappop(max_heap))
+
+            while max_heap and heap_dict[-max_heap[0]] > 0:
+                heap_dict[-max_heap[0]] -= 1
+                heappop(max_heap)
+            
+            while min_heap and heap_dict[min_heap[0]] > 0:
+                heap_dict[min_heap[0]] -= 1
+                heappop(min_heap)
+
+            median = self.find_median(max_heap, min_heap, k)
+            result.append(median)
+        
+        return result
