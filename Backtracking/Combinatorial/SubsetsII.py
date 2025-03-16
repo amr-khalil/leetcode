4dee1b553db68db69079354e37cca96645eed22a
Amr Khalil <amr.khalil@haufe-lexware.net>
2025-03-16 21:27:36 +0100

    Fix typo in Subsets II problem description

--- Backtracking/Combinatorial/SubsetsII.py
+++ Backtracking/Combinatorial/SubsetsII.py
@@ -1,6 +1,6 @@
 """
 Problem: Subsets II (with duplicates)"
-Given an integer array nums that may contain duplicates, return all possible subsets. The solution set must not contain duplicate subsets."
+Given an integer array nums that may contain duplicates, return all possible subsets. The solution set must not contain duplicate subsets."
 Example:
 Input: nums = [1, 2, 2]
 Output: [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]
